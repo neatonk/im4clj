560fd7edd653b8260f1f4ed56903b89afe90eb6e
Kevin Neaton <neatonk@gmail.com>
2012-03-05 18:29:02 -0600

    Add minus option aliases to magick-bindings...
    e.g. -write => write

--- src/im4clj/magick.clj
+++ src/im4clj/magick.clj
@@ -11,9 +11,20 @@
   im4clj.magick
   (:require [im4clj commands options]))
 
+(defn minus-option-aliases
+  [env]
+  (reduce (fn [keep [k v]]
+            (let [[minus? & newk] (name k)]
+              (if (= \- minus?)
+                (cons keep [(symbol (apply str newk)) v]))))
+          []
+          env))
+
 (def ^:private magick-bindings
-  (flatten (concat (ns-publics 'im4clj.commands)
-                   (ns-publics 'im4clj.options))))
+  (let [commands (ns-publics 'im4clj.commands)
+        options (ns-publics 'im4clj.options)
+        aliases (minus-option-aliases options)]
+    (flatten (concat commands options aliases))))
 
 (defmacro magick
   "Access to all of im4clj's goodness without polluting our current namespace.
