5f122020444815cbb3a87e19b3eb167ebb3b6ec1
Kevin Neaton <neatonk@gmail.com>
2012-03-07 09:48:27 -0600

    Attribute immigrate to Stephen C. Gilardi and Sam Aaron.

--- src/im4clj/util/ns.clj
+++ src/im4clj/util/ns.clj
@@ -6,6 +6,7 @@
  namespaces named by ns-names. The created vars have the same name, value
  and metadata as the original except that their :ns metadata value is this
  namespace."
+ {:author "Stephen C. Gilardi, Sam Aaron"}
  [& ns-names]
  (doseq [ns ns-names]
    (doseq [[sym var] (ns-publics ns)]
